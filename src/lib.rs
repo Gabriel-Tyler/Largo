2d5cebcfa99b6ac4399023b6ba4fb4d79d86b99c
Gabe <gabriel.tyler16@gmail.com>
2024-01-11 18:00:17 -0600

    Change Err to Error

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use std::io::Write;
 use std::{fmt, io};
 
 #[derive(Error, Debug)]
-enum Err {
+enum Error {
     #[error("{0}")]
     Reason(String),
     // #[error("Syntax error; line:{0} col:{1}")]
@@ -16,23 +16,23 @@ enum Err {
 }
 
 #[derive(Clone, Debug, PartialEq)]
-enum Exp {
+enum Expr {
     Symbol(String),
     Number(f64),
-    List(Vec<Exp>),
-    Func(fn(&[Exp]) -> Result<Exp>),
+    List(Vec<Expr>),
+    Func(fn(&[Expr]) -> Result<Expr>),
 }
 
-impl fmt::Display for Exp {
+impl fmt::Display for Expr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let repr = match self {
-            Exp::Symbol(s) => s.clone(),
-            Exp::Number(n) => n.to_string(),
-            Exp::List(l) => {
+            Expr::Symbol(s) => s.clone(),
+            Expr::Number(n) => n.to_string(),
+            Expr::List(l) => {
                 let l: Vec<String> = l.iter().map(|exp| exp.to_string()).collect();
                 format!("({})", l.join(","))
             }
-            Exp::Func(_) => "Function".to_owned(),
+            Expr::Func(_) => "Function".to_owned(),
         };
         write!(f, "{}", repr)
     }
@@ -40,7 +40,7 @@ impl fmt::Display for Exp {
 
 #[derive(Clone, Debug)]
 struct Env {
-    data: HashMap<String, Exp>,
+    data: HashMap<String, Expr>,
 }
 
 fn tokenize(expr: String) -> Vec<String> {
@@ -51,26 +51,26 @@ fn tokenize(expr: String) -> Vec<String> {
         .collect()
 }
 
-fn parse<'a>(tokens: &'a [String]) -> Result<(Exp, &'a [String])> {
+fn parse<'a>(tokens: &'a [String]) -> Result<(Expr, &'a [String])> {
     let (token, rest) = tokens
         .split_first()
-        .ok_or(Err::Reason("Could not get token".to_owned()))?;
+        .ok_or(Error::Reason("Could not get token".to_owned()))?;
     match token.as_str() {
         "(" => read_seq(rest),
-        ")" => Err(Err::Reason("Unexpected `)`".to_owned()).into()),
+        ")" => Err(Error::Reason("Unexpected `)`".to_owned()).into()),
         _ => Ok((parse_atom(token), rest)),
     }
 }
 
-fn read_seq<'a>(tokens: &'a [String]) -> Result<(Exp, &'a [String])> {
-    let mut result: Vec<Exp> = vec![];
+fn read_seq<'a>(tokens: &'a [String]) -> Result<(Expr, &'a [String])> {
+    let mut result: Vec<Expr> = vec![];
     let mut xs = tokens;
     loop {
         let (next_token, rest) = xs
             .split_first()
-            .ok_or(Err::Reason("Could not find closing `)`".to_owned()))?;
+            .ok_or(Error::Reason("Could not find closing `)`".to_owned()))?;
         if next_token == ")" {
-            return Ok((Exp::List(result), rest));
+            return Ok((Expr::List(result), rest));
         }
         let (exp, new_xs) = parse(&xs)?;
         result.push(exp);
@@ -78,94 +78,94 @@ fn read_seq<'a>(tokens: &'a [String]) -> Result<(Exp, &'a [String])> {
     }
 }
 
-fn parse_atom(token: &str) -> Exp {
+fn parse_atom(token: &str) -> Expr {
     let parse_result = token.parse();
     match parse_result {
-        Ok(v) => Exp::Number(v),
-        Err(_) => Exp::Symbol(token.to_owned()),
+        Ok(v) => Expr::Number(v),
+        Err(_) => Expr::Symbol(token.to_owned()),
     }
 }
 
 fn default_env() -> Env {
     // `data` is a map from symbols to expressions
-    let mut data = HashMap::<String, Exp>::new();
+    let mut data = HashMap::<String, Expr>::new();
     data.insert(
         "+".to_owned(),
-        Exp::Func(|args: &[Exp]| -> Result<Exp> {
+        Expr::Func(|args: &[Expr]| -> Result<Expr> {
             let floats = parse_list_of_floats(args)?;
             let sum: f64 = floats.iter().sum();
-            Ok(Exp::Number(sum))
+            Ok(Expr::Number(sum))
         }),
     );
     data.insert(
         "-".to_owned(),
-        Exp::Func(|args: &[Exp]| -> Result<Exp> {
+        Expr::Func(|args: &[Expr]| -> Result<Expr> {
             let floats = parse_list_of_floats(args)?;
             let &first = floats
                 .first()
-                .ok_or(Err::Reason("`-` requires at least one operand".to_owned()))?;
+                .ok_or(Error::Reason("`-` requires at least one operand".to_owned()))?;
             let sum_rest: f64 = floats.iter().skip(1).sum();
-            Ok(Exp::Number(first - sum_rest))
+            Ok(Expr::Number(first - sum_rest))
         }),
     );
     Env { data }
 }
 
-fn parse_list_of_floats(floats: &[Exp]) -> Result<Vec<f64>> {
+fn parse_list_of_floats(floats: &[Expr]) -> Result<Vec<f64>> {
     floats.iter().map(|exp| parse_single_float(exp)).collect()
 }
 
-fn parse_single_float(exp: &Exp) -> Result<f64> {
+fn parse_single_float(exp: &Expr) -> Result<f64> {
     match exp {
-        Exp::Number(num) => Ok(*num),
-        _ => Err(Err::Reason("Expected a number".to_owned()))?,
+        Expr::Number(num) => Ok(*num),
+        _ => Err(Error::Reason("Expected a number".to_owned()))?,
     }
 }
 
-fn eval(exp: &Exp, env: &mut Env) -> Result<Exp> {
+fn eval(exp: &Expr, env: &mut Env) -> Result<Expr> {
     match exp {
         // lookup symbol
-        Exp::Symbol(symbol) => Ok(env
+        Expr::Symbol(symbol) => Ok(env
             .data
             .get(symbol)
-            .ok_or(Err::Reason(format!("Unexpected symbol `{symbol}`")))
+            .ok_or(Error::Reason(format!("Unexpected symbol `{symbol}`")))
             .cloned()?),
 
         // return the number
-        Exp::Number(_) => Ok(exp.clone()),
+        Expr::Number(_) => Ok(exp.clone()),
 
         // evaluate each item in list and apply
-        Exp::List(list) => {
+        Expr::List(list) => {
             // get car and cdr
             let (op, args) = list
                 .split_first()
-                .ok_or(Err::Reason("Expected non-empty list".to_owned()))?;
+                .ok_or(Error::Reason("Expected non-empty list".to_owned()))?;
 
             // evaluate the operator
             let op = eval(op, env)?;
 
             // check that op is a function
             match op {
-                Exp::Func(op) => {
+                Expr::Func(op) => {
                     // evaluate args
                     let args = args
                         .iter()
                         .map(|x| eval(x, env))
-                        .collect::<Result<Vec<Exp>>>()?;
+                        .collect::<Result<Vec<Expr>>>()?;
 
                     // apply
                     op(&args)
                 }
-                _ => Err(Err::Reason("Operator must be a function".to_owned()).into()),
+                _ => Err(Error::Reason("Operator must be a function".to_owned()).into()),
             }
         }
 
         // shouldn't be allowed
-        Exp::Func(_) => Err(Err::Reason("Cannot evaluate a function".to_owned()).into()),
+        Expr::Func(_) => Err(Error::Reason("Cannot evaluate a function".to_owned()).into()),
     }
 }
 
-fn string_to_exp(lexemes: String, env: &mut Env) -> Result<Exp> {
+fn string_to_exp(lexemes: String, env: &mut Env) -> Result<Expr> {
     let (parsed, _) = parse(&tokenize(lexemes))?;
     let expr = eval(&parsed, env)?;
     Ok(expr)
@@ -202,8 +202,8 @@ mod tests {
     #[test]
     fn check_error() {
         assert_eq!(
-            "Error: Hello there".to_owned(),
-            format!("{}", Err::Reason("Hello there".to_owned()))
+            "Hello there".to_owned(),
+            format!("{}", Error::Reason("Hello there".to_owned()))
         );
     }
 
@@ -228,10 +228,10 @@ mod tests {
         let (exp, rest) = parse(tokens.as_slice()).unwrap();
         assert_eq!(
             exp,
-            Exp::List(vec![
-                Exp::Symbol("+".to_owned()),
-                Exp::Number(1.0),
-                Exp::Number(2.0),
+            Expr::List(vec![
+                Expr::Symbol("+".to_owned()),
+                Expr::Number(1.0),
+                Expr::Number(2.0),
             ])
         );
         assert!(rest.is_empty());
@@ -239,9 +239,9 @@ mod tests {
 
     #[test]
     fn check_parse_atom() {
-        assert_eq!(parse_atom("1.0"), Exp::Number(1.0));
-        assert_eq!(parse_atom("Hello"), Exp::Symbol("Hello".to_owned()));
-        assert_eq!(parse_atom("hi1.0hi"), Exp::Symbol("hi1.0hi".to_owned()));
+        assert_eq!(parse_atom("1.0"), Expr::Number(1.0));
+        assert_eq!(parse_atom("Hello"), Expr::Symbol("Hello".to_owned()));
+        assert_eq!(parse_atom("hi1.0hi"), Expr::Symbol("hi1.0hi".to_owned()));
     }
 
     #[test]
@@ -249,33 +249,33 @@ mod tests {
         let Env { data } = default_env();
 
         let add = *match data.get("+").unwrap() {
-            Exp::Func(f) => f,
+            Expr::Func(f) => f,
             _ => panic!("data did not return addition"),
         };
         let sub = *match data.get("-").unwrap() {
-            Exp::Func(f) => f,
+            Expr::Func(f) => f,
             _ => panic!("data did not return subtraction"),
         };
 
-        let exps = vec![Exp::Number(1.0), Exp::Number(2.0), Exp::Number(3.0)];
+        let exps = vec![Expr::Number(1.0), Expr::Number(2.0), Expr::Number(3.0)];
 
-        assert_eq!(add(&exps).unwrap(), Exp::Number(6.0));
-        assert_eq!(sub(&exps).unwrap(), Exp::Number(-4.0));
+        assert_eq!(add(&exps).unwrap(), Expr::Number(6.0));
+        assert_eq!(sub(&exps).unwrap(), Expr::Number(-4.0));
     }
 
     #[test]
     fn check_eval() {
         let mut env = default_env();
 
-        // Exp::List
+        // Expr::List
         let (exp1, _) = parse(&tokenize("(+ 1 2)".to_owned())).unwrap();
         let (exp2, _) = parse(&tokenize("(+ 1 (+ 2 3 4))".to_owned())).unwrap();
         let (exp3, _) = parse(&tokenize("(- 2 3)".to_owned())).unwrap();
         let (exp4, _) = parse(&tokenize("(- 2 (+ 1 2 3))".to_owned())).unwrap();
 
-        assert_eq!(eval(&exp1, &mut env).unwrap(), Exp::Number(3.0));
-        assert_eq!(eval(&exp2, &mut env).unwrap(), Exp::Number(10.0));
-        assert_eq!(eval(&exp3, &mut env).unwrap(), Exp::Number(-1.0));
-        assert_eq!(eval(&exp4, &mut env).unwrap(), Exp::Number(-4.0));
+        assert_eq!(eval(&exp1, &mut env).unwrap(), Expr::Number(3.0));
+        assert_eq!(eval(&exp2, &mut env).unwrap(), Expr::Number(10.0));
+        assert_eq!(eval(&exp3, &mut env).unwrap(), Expr::Number(-1.0));
+        assert_eq!(eval(&exp4, &mut env).unwrap(), Expr::Number(-4.0));
     }
 }
